3519f0996101bb32d407ef769d28d03c02b85560
Eduardo - EPS Tecnologia <edupereira027@gmail.com>
2026-06-14 11:48:19 -0300

    Criação de novas funções na rota livros

--- src/app.js
+++ src/app.js
@@ -9,6 +9,12 @@ const livros = [
   { id: 3, titulo: 'O Código Da Vinci', autor: 'Dan Brown' },
 ];
 
+function buscaLivro(id) {
+ return livros.findIndex(livro => {
+   return livro.id === Number(id);
+ })
+}
+
 app.get('/', (req, res) => {
   res.status(200).send('Cuso de Node.js - Express e MongoDB');
 });
@@ -20,6 +26,17 @@ app.get('/livros', (req, res) => {
 app.post('/livros', (req, res) => {
   livros.push(req.body);
   res.status(201).send('Livro adicionado com sucesso!');
+});
+
+app.get("/livros/:id", (req, res) => {
+ const index = buscaLivro(req.params.id);
+ res.status(200).json(livros[index]);
+});
+
+app.put("/livros/:id", (req, res) => {
+ const index = buscaLivro(req.params.id);
+ livros[index].titulo = req.body.titulo;
+ res.status(200).json(livros);
 });
 
 export default app;
